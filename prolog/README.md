94d9d37f234c3e3ee820e4102344a4666999049a
Mads Buch <madspbuch@gmail.com>
2016-01-24 23:59:20 +0100

    description

--- prolog/README.md
+++ prolog/README.md
@@ -1,1 +1,22 @@
 # Prolog Fibonacci Implementation
+
+## Running
+Start the swipl interpreter reading the initial propositions:
+
+        $ swipl fib.pl
+
+the direct recursive implementation is evoked as
+
+        ?- fib_direct(10, X).
+        X = 55 .
+
+Use the wrapper for the implementation based in peano naturals
+
+        ?- fib_peano_wrapper_get_res(10, R).
+        R = 55 .
+
+And retrieving the argument for a given Fibonacci number
+
+        ?- fib_peano_wrapper_get_arg(A, 55).
+        A = 10 .
+
--- prolog/fib.pl
+++ prolog/fib.pl
@@ -3,11 +3,11 @@
 fib_direct(0, 0).
 fib_direct(1, 1).
 fib_direct(N, R) :-
-	X1 is N-1,
-	X2 is N-2,
-	fib_direct(X1, A),
-	fib_direct(X2, B),
-	R is A + B.
+    X1 is N-1,
+    X2 is N-2,
+    fib_direct(X1, A),
+    fib_direct(X2, B),
+    R is A + B.
 
 % Definiition of the Peano numbers
 nat(0).
@@ -16,13 +16,13 @@ nat(s(_)).
 % Readable nats
 nat_to_int(0, 0).
 nat_to_int(s(N), R) :-
-	nat_to_int(N, R1),
-	R is R1 + 1.
+    nat_to_int(N, R1),
+    R is R1 + 1.
 
 int_to_nat(0, 0).
 int_to_nat(X, s(R)) :-
-	X1 is X-1,
-	int_to_nat(X1, R).
+    X1 is X-1,
+    int_to_nat(X1, R).
 
 % Definition of addition ( add(A, B, Result ))
 add(0, B, B).
@@ -31,16 +31,16 @@ add(s(A), B, R) :- add(A, s(B), R).
 fib_peano(0, 0).
 fib_peano(s(0), s(0)).
 fib_peano(s(s(N)), R) :-
-	fib_peano(N, A),
-	fib_peano(s(N), B),
-	add(A, B, R).
+    fib_peano(N, A),
+    fib_peano(s(N), B),
+    add(A, B, R).
 
 % Wrapper functions dealing with all the conversions
 fib_peano_wrapper_get_res(A, B) :-
-	int_to_nat(A, X),
-	fib_peano(X, Y),
-	nat_to_int(Y, B).
+    int_to_nat(A, X),
+    fib_peano(X, Y),
+    nat_to_int(Y, B).
 fib_peano_wrapper_get_arg(A, B) :-
-	int_to_nat(B, Y),
-	fib_peano(X, Y),
-	nat_to_int(X, A).
+    int_to_nat(B, Y),
+    fib_peano(X, Y),
+    nat_to_int(X, A).
